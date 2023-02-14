ff01cc94e2cc73dc194eb3f986e2f081d1c62185
giorgiacucino <73819212+giorgiacucino@users.noreply.github.com>
2023-02-14 11:12:10 +0100

    add jokes.sh

---
+++ joke.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl ascii.live/rick &
+while [ 1 ]
+do
+	say "Never gonna give you up"
+	say "Never gonna let you down"
+	say "Never gonna run around and desert you"
+	say "Never gonna make you cry"
+	say "Never gonna say goodbye"
+	say "Never gonna tell a lie and hurt you"
+done
